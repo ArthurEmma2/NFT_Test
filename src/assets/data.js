d731a14b92bd028dc3ffb3c614b43de51c8e75af
Arthur Emmanuel <yhawnoworries@gmail.com>
2023-03-08 07:33:56 +0000

    update

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,108 +1,95 @@
+import img01 from "./img/img1.avif";
+import img02 from "./img/img07.avif";
+import img03 from "./img/gn_ape.avif";
+import img04 from "./img/gn_ape1.avif";
 
-import img01 from './img/img1.avif'
-import img02 from './img/img07.avif'
-import img03 from './img/gn_ape.avif'
-import img04 from './img/gn_ape1.avif'
-
-import img05 from './img/explore_1.avif'
-import img06 from './img/img04.avif'
-import img07 from './img/clone.avif'
-import img08 from './img/clone_1.avif'
-
-
-
-
+import img05 from "./img/explore_1.avif";
+import img06 from "./img/img04.avif";
+import img07 from "./img/clone.avif";
+import img08 from "./img/clone_1.avif";
 
 export const NFT__DATA = [
-    {
-      id: "01",
-      name: "Hooligan #8109",
-      price: "0.874 ETH",
-      desc: "Web3 finds its heroes in the most unlikely of characters. A genesis NFT collection of 8888 Pet Hooligans – unique 3D characters that are posed to defend Web3 from the corporate overlord, Metazuckbot. Hooligans are built with a wide variety of traits on the Ethereum blockchain, and provide utility unlike any other NFT collection.Each Hooligan provides access to the AMGI Studios' suite of animation products, and are also your unique playable 3D avatar in our PvP and social metaverse, The Rabbit Hole - alpha release out now! Sequi Lepus.",
-      imgUrl: img01,
-      address: "0xE0Fa...526b",
-      link: "https://opensea.io/assets/ethereum/0x09233d553058c2f42ba751c87816a8e9fae7ef10/8109",
-      category: "Gaming"
-    },
-    {
-      id: "02",
-      name: "Hooligan #2324",
-      desc: "Web3 finds its heroes in the most unlikely of characters. A genesis NFT collection of 8888 Pet Hooligans – unique 3D characters that are posed to defend Web3 from the corporate overlord, Metazuckbot. Hooligans are built with a wide variety of traits on the Ethereum blockchain, and provide utility unlike any other NFT collection.Each Hooligan provides access to the AMGI Studios' suite of animation products, and are also your unique playable 3D avatar in our PvP and social metaverse, The Rabbit Hole - alpha release out now! Sequi Lepus.",
-      imgUrl: img02,
-      link: 'https://opensea.io/assets/ethereum/0x09233d553058c2f42ba751c87816a8e9fae7ef10/2324',
-      address: "0xE0Fa...526b",
-      price: "0.9899 ETH",
-      category: "Gaming"
+  {
+    id: "01",
+    name: "Hooligan #8109",
+    price: "0.874 ETH",
+    desc: "Web3 finds its heroes in the most unlikely of characters. A genesis NFT collection of 8888 Pet Hooligans – unique 3D characters that are posed to defend Web3 from the corporate overlord, Metazuckbot. Hooligans are built with a wide variety of traits on the Ethereum blockchain, and provide utility unlike any other NFT collection.Each Hooligan provides access to the AMGI Studios' suite of animation products, and are also your unique playable 3D avatar in our PvP and social metaverse, The Rabbit Hole - alpha release out now! Sequi Lepus.",
+    imgUrl: img01,
+    address: "0xE0Fa...526b",
+    link: "https://opensea.io/assets/ethereum/0x09233d553058c2f42ba751c87816a8e9fae7ef10/8109",
+    category: "Gaming",
+  },
+  {
+    id: "02",
+    name: "Hooligan #2324",
+    desc: "Web3 finds its heroes in the most unlikely of characters. A genesis NFT collection of 8888 Pet Hooligans – unique 3D characters that are posed to defend Web3 from the corporate overlord, Metazuckbot. Hooligans are built with a wide variety of traits on the Ethereum blockchain, and provide utility unlike any other NFT collection.Each Hooligan provides access to the AMGI Studios' suite of animation products, and are also your unique playable 3D avatar in our PvP and social metaverse, The Rabbit Hole - alpha release out now! Sequi Lepus.",
+    imgUrl: img02,
+    link: "https://opensea.io/assets/ethereum/0x09233d553058c2f42ba751c87816a8e9fae7ef10/2324",
+    address: "0xE0Fa...526b",
+    price: "0.9899 ETH",
+    category: "Gaming",
+  },
 
-    },
-  
-    {
-      id: "03",
-      name: "GN Ape House #4294",
-      desc: "The GN Ape House, is a Collection of 4,786 unique ape NFTs, living on the Polygon blockchain.P2E Coming Soon! https://gnapehouse.com/",
-      imgUrl: img03,
-      category:"Gaming",
-      link: "https://opensea.io/assets/matic/0x0cedf6129f9a20109f0e0f043c1e01590a2d6ca6/4294",
-      price: "3.2 MATIC",
-      address: "0xD9DA...600e",
-    },
-  
-    
-     
-  
-    {
-      id: "04",
-      name: "GN Ape House #135",
-      desc: "The GN Ape House, is a Collection of 4,786 unique ape NFTs, living on the Polygon blockchain.P2E Coming Soon! https://gnapehouse.com/",
-      imgUrl: img04,
-      link: "https://opensea.io/assets/matic/0x0cedf6129f9a20109f0e0f043c1e01590a2d6ca6/135",
-      price: "3.5 MATIC" ,
-      address: "0xD9DA...600e",
-      category:"Gaming",
-    },
-    {
-      id: "05",
-      name: "Explore 10:52:07",
-      desc: "In the face of such grandeur there is a question of what it is to be human and how we are reminded of how very small we stand; a tiny microcosm in the face of such vast and magnificent natural and urban wonder.",
-      imgUrl: img05,
-      link: "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/74560649326607298578853393268143794665781666052470848563380919802270515724289",
-      price: "not available" ,
-      address: "0x5492...3360",
-      category:"photography",
-    },
-    {
-      id: "06",
-      name: "Hooligan #667",
-      link: "https://opensea.io/assets/ethereum/0x09233d553058c2f42ba751c87816a8e9fae7ef10/667",
-      imgUrl: img06,
-      desc: "Web3 finds its heroes in the most unlikely of characters. A genesis NFT collection of 8888 Pet Hooligans – unique 3D characters that are posed to defend Web3 from the corporate overlord, Metazuckbot. Hooligans are built with a wide variety of traits on the Ethereum blockchain, and provide utility unlike any other NFT collection.Each Hooligan provides access to the AMGI Studios' suite of animation products, and are also your unique playable 3D avatar in our PvP and social metaverse, The Rabbit Hole - alpha release out now! Sequi Lepus.",
-      price: "0.937 ETH" ,
-      address: "0xE0Fa...526b",
-      category:"Gaming",
-    },
-    {
-      id: "07",
-      name: "CloneX #17655",
-      link: "https://opensea.io/assets/ethereum/0x49cf6f5d44e70224e2e23fdcdd2c053f30ada28b/5588",
-      imgUrl: img07,
-      desc: "20,000 next-gen Avatars, by RTFKT and Takashi Murakami",
-      price: "4.575 ETH" ,
-      address: "0xE0Fa...526b",
-      category:"PFPs",
-    },
-    {
-      id: "08",
-      name: "CloneX #16786",
-      link: "https://opensea.io/assets/ethereum/0x49cf6f5d44e70224e2e23fdcdd2c053f30ada28b/6228",
-      imgUrl: img08,
-      desc: "20,000 next-gen Avatars, by RTFKT and Takashi Murakami",
-      price: "4.575 ETH" ,
-      address: "0xE0Fa...526b",
-      category:"PFPs",
-    },
-  
-  
-  
-   
-  ];
-  
+  {
+    id: "03",
+    name: "GN Ape House #4294",
+    desc: "The GN Ape House, is a Collection of 4,786 unique ape NFTs, living on the Polygon blockchain.P2E Coming Soon! https://gnapehouse.com/",
+    imgUrl: img03,
+    category: "Gaming",
+    link: "https://opensea.io/assets/matic/0x0cedf6129f9a20109f0e0f043c1e01590a2d6ca6/4294",
+    price: "3.2 MATIC",
+    address: "0xD9DA...600e",
+  },
+
+  {
+    id: "04",
+    name: "GN Ape House #135",
+    desc: "The GN Ape House, is a Collection of 4,786 unique ape NFTs, living on the Polygon blockchain.P2E Coming Soon! https://gnapehouse.com/",
+    imgUrl: img04,
+    link: "https://opensea.io/assets/matic/0x0cedf6129f9a20109f0e0f043c1e01590a2d6ca6/135",
+    price: "3.5 MATIC",
+    address: "0xD9DA...600e",
+    category: "Gaming",
+  },
+  {
+    id: "05",
+    name: "Explore 10:52:07",
+    desc: "In the face of such grandeur there is a question of what it is to be human and how we are reminded of how very small we stand; a tiny microcosm in the face of such vast and magnificent natural and urban wonder.",
+    imgUrl: img05,
+    link: "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/74560649326607298578853393268143794665781666052470848563380919802270515724289",
+    price: "not available",
+    address: "0x5492...3360",
+    category: "photography",
+  },
+  {
+    id: "06",
+    name: "Hooligan #667",
+    link: "https://opensea.io/assets/ethereum/0x09233d553058c2f42ba751c87816a8e9fae7ef10/667",
+    imgUrl: img06,
+    desc: "Web3 finds its heroes in the most unlikely of characters. A genesis NFT collection of 8888 Pet Hooligans – unique 3D characters that are posed to defend Web3 from the corporate overlord, Metazuckbot. Hooligans are built with a wide variety of traits on the Ethereum blockchain, and provide utility unlike any other NFT collection.Each Hooligan provides access to the AMGI Studios' suite of animation products, and are also your unique playable 3D avatar in our PvP and social metaverse, The Rabbit Hole - alpha release out now! Sequi Lepus.",
+    price: "0.937 ETH",
+    address: "0xE0Fa...526b",
+    category: "Gaming",
+  },
+  {
+    id: "07",
+    name: "CloneX #17655",
+    link: "https://opensea.io/assets/ethereum/0x49cf6f5d44e70224e2e23fdcdd2c053f30ada28b/5588",
+    imgUrl: img07,
+    desc: "20,000 next-gen Avatars, by RTFKT and Takashi Murakami",
+    price: "4.575 ETH",
+    address: "0xE0Fa...526b",
+    category: "PFPs",
+  },
+
+  {
+    id: "08",
+    name: "CloneX #16786",
+    link: "https://opensea.io/assets/ethereum/0x49cf6f5d44e70224e2e23fdcdd2c053f30ada28b/6228",
+    imgUrl: img08,
+    desc: "20,000 next-gen Avatars, by RTFKT and Takashi Murakami",
+    price: "4.575 ETH",
+    address: "0xE0Fa...526b",
+    category: "PFPs",
+  },
+];
